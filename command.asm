4fa7ba82794ab9422c4dd878dbe45e9125a291a5
ybuzoku <ybuzoku@hotmail.co.uk>
2023-01-13 15:23:51 +0000

    Reorganised the build tree. Build an image that matches the previous image

--- command.asm
+++ command.asm
@@ -3,26 +3,26 @@
 [DEFAULT REL]
 BITS 64
 ;Defs, strucs and macros
-%include "./Source/Include/dosMacro.mac"
-%include "./Source/Include/dosStruc.inc"
-%include "./Source/Include/fcbStruc.inc"
-%include "./Source/Include/dosError.inc"
-%include "./Utils/COMMAND/Data/cmdEqu.asm"
+%include "./Include/dosMacro.mac"
+%include "./Include/dosStruc.inc"
+%include "./Include/fcbStruc.inc"
+%include "./Include/dosError.inc"
+%include "./Include/cmdEqu.inc"
 Segment .data align=1 
-%include "./Utils/COMMAND/Data/cmdData.asm"
-%include "./Utils/COMMAND/Data/cmdTable.asm"
-%include "./Utils/COMMAND/Data/cmdMsg.asm"
-%include "./Utils/COMMAND/Source/cmdMain.asm"
-%include "./Utils/COMMAND/Source/cmdFunc.asm"
-%include "./Utils/COMMAND/Source/cmdUtils.asm"
-%include "./Utils/COMMAND/Source/int44h.asm"
+%include "./Data/cmdData.asm"
+%include "./Data/cmdTable.asm"
+%include "./Data/cmdMsg.asm"
+%include "./Source/cmdMain.asm"
+%include "./Source/cmdFunc.asm"
+%include "./Source/cmdUtils.asm"
+%include "./Source/int44h.asm"
 
 endOfAllocNoMaster: ;End of alloc if not the master cmd
-%include "./Utils/COMMAND/Data/cmdEnv.asm"
+%include "./Data/cmdEnv.asm"
 endOfAlloc: ;End of alloc if the master cmd
 ;The stack is always setup one stackSize away from the endOfAlloc, aligned to
 Segment transient align=1 follows=.data
 ;This segment always gets ejected post load
-%include "./Utils/COMMAND/Source/cmdLdr.asm"
+%include "./Source/cmdLdr.asm"
 
 stackSize equ 64
