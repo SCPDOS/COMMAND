b8f1ca3d2454e72c0bb184d7aef52bbaaf3c6ac6
ybuzoku <ybuzoku@hotmail.co.uk>
2025-01-04 00:00:50 +0000

    Split the cmdfunc file into two function files and and error file.

---
+++ src/cmdErr.asm
@@ -0,0 +1,50 @@
+;This file contains the error printing routines
+;Common Error Messages, jumped to to return from
+badDiskFull:
+;This is a different error return, as this closes handles and prints
+; disk full error and resets the command line!
+    lea rdx, noSpace
+    lea rcx, pipeErr
+    test byte [pipeFlag], -1    ;Is the flag set
+    jz short badCmn ;If its just disk full, dont go through pipe clean
+    cmovnz rdx, rcx ;Swap error messages if pipe flag is on
+    call badCmn     ;Print the string
+    jmp redirPipeFailureCommon.noPrint  ;Now close pipes and fully reset!
+badNoMemError:
+    lea rdx, noMemMsg
+    jmp short badCmn
+badSyntaxError:
+    lea rdx, syntaxErr
+    jmp short badCmn
+badEnvSpaceError:
+    lea rdx, noEnvSpace
+    jmp short badCmn
+badAccError:
+    lea rdx, accDenMsg
+    jmp short badCmn
+badParamError:
+    lea rdx, badParm
+    jmp short badCmn
+badDriveError:
+    lea rdx, badDrv
+    jmp short badCmn
+badArgError:
+    lea rdx, badArgs
+    jmp short badCmn
+badFileError:
+    lea rdx, badSpec
+    jmp short badCmn
+badDupFnf:
+;Hybrid error message
+    lea rdx, dupName
+    jmp short badCmn
+badFnf:
+    lea rdx, fnfMsg
+    jmp short badCmn
+badDirError:
+    lea rdx, badDir
+badCmn:
+    jmp printString     ;This will be made nuanced later, to agree with DOS
+badCmdError:
+    lea rdx, badCmd
+    jmp short badCmn
